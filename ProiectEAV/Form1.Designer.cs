6595da456498d837dfafe6c336d500eef671acba
pade88 <ovidiu.padeanu@gmail.com>
2020-12-30 23:36:12 +0200

    Add project files.

---
+++ ProiectEAV/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace ProiectEAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView1 = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
+            this.OficiiPostale = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCautare = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.webView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView1
+            // 
+            this.webView1.Location = new System.Drawing.Point(12, 12);
+            this.webView1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webView1.Name = "webView1";
+            this.webView1.Size = new System.Drawing.Size(953, 722);
+            this.webView1.TabIndex = 0;
+            // 
+            // OficiiPostale
+            // 
+            this.OficiiPostale.FormattingEnabled = true;
+            this.OficiiPostale.Location = new System.Drawing.Point(971, 41);
+            this.OficiiPostale.Name = "OficiiPostale";
+            this.OficiiPostale.Size = new System.Drawing.Size(272, 21);
+            this.OficiiPostale.TabIndex = 1;
+            this.OficiiPostale.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(972, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Oficiu Postal";
+            // 
+            // textBoxCautare
+            // 
+            this.textBoxCautare.Location = new System.Drawing.Point(971, 92);
+            this.textBoxCautare.Name = "textBoxCautare";
+            this.textBoxCautare.Size = new System.Drawing.Size(272, 20);
+            this.textBoxCautare.TabIndex = 3;
+            this.textBoxCautare.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(971, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Street View";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(971, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cautare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 746);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxCautare);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OficiiPostale);
+            this.Controls.Add(this.webView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Toolkit.Forms.UI.Controls.WebView webView1;
+        private System.Windows.Forms.ComboBox OficiiPostale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCautare;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProiectEAV/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProiectEAV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Default home page
+            webView1.Navigate("https://www.google.ro/maps/search/oficii+postale+craiova/@44.3080085,23.767188,13z/data=!3m1!4b1?hl=ro");
+        }
+
+        DataTable dt = new DataTable();
+        public void LoadData()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-CUHAUG6\SQLEXPRESS;" +
+                "Initial Catalog=PostOficces;Integrated Security=SSPI;");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM oficii_postale", conn);
+            SqlDataAdapter sa = new SqlDataAdapter(cmd);
+            conn.Open();
+            sa.Fill(dt);
+            conn.Close();
+            sa.Dispose();
+            cmd.Dispose();
+            conn.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                OficiiPostale.Items.Add(Convert.ToString(dt.Rows[i]["denumire"]));
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (OficiiPostale.Text == Convert.ToString(dt.Rows[i]["denumire"]))
+                {
+                    webView1.Navigate(Convert.ToString(dt.Rows[i]["link_gm"]));
+                    break;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string StringComp = "https://www.google.com/maps/search/" + textBoxCautare.Text;
+            webView1.Navigate(StringComp);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (OficiiPostale.Text == Convert.ToString(dt.Rows[i]["denumire"]))
+                {
+                    webView1.Navigate(Convert.ToString(dt.Rows[i]["link_gsv"]));
+                    break;
+                }
+            }
+        }
+    }
+}
